65f25aff66698c20a10717d2db3f52135030899a
Werenyckyj Vojtěch <werenyckyj.vo.2019@skola.ssps.cz>
2021-09-20 12:59:41 +0200

    Přidejte soubory projektu.

---
+++ 200921c1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("200921c1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("200921c1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("91c4527b-8970-4609-b141-db33ab1eaf30")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
+// pomocí zástupného znaku * takto:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
